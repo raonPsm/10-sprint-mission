9d61120f3e8ededc6fa72a9655954dad216e23ab
raonPsm <s102075@naver.com>
2026-03-08 21:27:20 +0900

    feat: BinaryContentController - 파일을 다운로드하는 API 추가

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -2,8 +2,8 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.BinaryContentApi;
 import com.sprint.mission.discodeit.dto.Dto.BinaryContentDto;
-import com.sprint.mission.discodeit.mapper.BinaryContentMapper;
 import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +16,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class BinaryContentController implements BinaryContentApi {
     private final BinaryContentService binaryContentService;
-    private final BinaryContentMapper binaryContentMapper;
+    private final BinaryContentStorage binaryContentStorage;
 
     // GET /api/binaryContents - 여러 첨부 파일 조회
     @GetMapping
@@ -32,5 +32,12 @@ public class BinaryContentController implements BinaryContentApi {
     public ResponseEntity<BinaryContentDto> find(@PathVariable UUID binaryContentId) {
         BinaryContentDto binaryContent = binaryContentService.find(binaryContentId);
         return ResponseEntity.ok(binaryContent);
+    }
+
+    // GET /api/{binaryContentId}/download - 파일 다운로드
+    @GetMapping("{binaryContentId}/download")
+    public ResponseEntity<?> download(@PathVariable UUID binaryContentId) {
+        BinaryContentDto binaryContentDto = binaryContentService.find(binaryContentId);
+        return binaryContentStorage.download(binaryContentDto);
     }
 }
