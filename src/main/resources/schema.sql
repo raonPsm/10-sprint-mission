295bf480f267fb4d7fc9ef4e7d47159830b59f70
raonPsm <s102075@naver.com>
2026-03-10 14:10:42 +0900

    Comment: 설정 파일 주석 설명 주석 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -19,74 +19,74 @@ DROP TABLE IF EXISTS binary_contents;
 --
 
 CREATE TABLE binary_contents (
-                                 id UUID PRIMARY KEY,
-                                 created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-                                 file_name VARCHAR(255) NOT NULL,
-                                 size BIGINT NOT NULL,
-                                 content_type VARCHAR(100) NOT NULL
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    file_name VARCHAR(255) NOT NULL,
+    size BIGINT NOT NULL,
+    content_type VARCHAR(100) NOT NULL
     -- bytes BYTEA NOT NULL
 );
 
 CREATE TABLE users (
-                       id UUID PRIMARY KEY,
-                       created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-                       updated_at TIMESTAMP WITH TIME ZONE,
-                       username VARCHAR(50) NOT NULL UNIQUE,
-                       email VARCHAR(100) NOT NULL UNIQUE,
-                       password VARCHAR(60) NOT NULL,
-                       profile_id UUID UNIQUE,
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL,
+    profile_id UUID UNIQUE,
 
-                       FOREIGN KEY (profile_id) REFERENCES binary_contents(id) ON DELETE SET NULL
+    FOREIGN KEY (profile_id) REFERENCES binary_contents(id) ON DELETE SET NULL
 );
 
 CREATE TABLE user_statuses (
-                               id UUID PRIMARY KEY,
-                               created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-                               updated_at TIMESTAMP WITH TIME ZONE,
-                               user_id UUID NOT NULL UNIQUE,
-                               last_active_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    user_id UUID NOT NULL UNIQUE,
+    last_active_at TIMESTAMP WITH TIME ZONE NOT NULL,
 
-                               FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE channels (
-                          id UUID PRIMARY KEY,
-                          created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-                          updated_at TIMESTAMP WITH TIME ZONE,
-                          name VARCHAR(100),
-                          description VARCHAR(500),
-                          type VARCHAR(10) NOT NULL CHECK (type IN ('PUBLIC', 'PRIVATE'))
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    name VARCHAR(100),
+    description VARCHAR(500),
+    type VARCHAR(10) NOT NULL CHECK (type IN ('PUBLIC', 'PRIVATE'))
 );
 
 CREATE TABLE messages (
-                          id UUID PRIMARY KEY,
-                          created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-                          updated_at TIMESTAMP WITH TIME ZONE,
-                          content TEXT,
-                          channel_id UUID NOT NULL,
-                          author_id UUID,
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    content TEXT,
+    channel_id UUID NOT NULL,
+    author_id UUID,
 
-                          FOREIGN KEY (channel_id) REFERENCES channels(id) ON DELETE CASCADE,
-                          FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE SET NULL
+    FOREIGN KEY (channel_id) REFERENCES channels(id) ON DELETE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE SET NULL
 );
 
 CREATE TABLE read_statuses (
-                               id UUID PRIMARY KEY,
-                               created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-                               updated_at TIMESTAMP WITH TIME ZONE,
-                               user_id UUID NOT NULL,
-                               channel_id UUID NOT NULL,
-                               last_read_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    user_id UUID NOT NULL,
+    channel_id UUID NOT NULL,
+    last_read_at TIMESTAMP WITH TIME ZONE NOT NULL,
 
-                               FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-                               FOREIGN KEY (channel_id) REFERENCES channels(id) ON DELETE CASCADE,
-                               CONSTRAINT uk_user_channel UNIQUE (user_id, channel_id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (channel_id) REFERENCES channels(id) ON DELETE CASCADE,
+    CONSTRAINT uk_user_channel UNIQUE (user_id, channel_id)
 );
 
 CREATE TABLE message_attachments (
-                                     message_id UUID NOT NULL,
-                                     attachment_id UUID NOT NULL,
-                                     PRIMARY KEY (message_id, attachment_id),
-                                     FOREIGN KEY (message_id) REFERENCES messages(id) ON DELETE CASCADE,
-                                     FOREIGN KEY (attachment_id) REFERENCES binary_contents(id) ON DELETE CASCADE
+    message_id UUID NOT NULL,
+    attachment_id UUID NOT NULL,
+    PRIMARY KEY (message_id, attachment_id),
+    FOREIGN KEY (message_id) REFERENCES messages(id) ON DELETE CASCADE,
+    FOREIGN KEY (attachment_id) REFERENCES binary_contents(id) ON DELETE CASCADE
 );
