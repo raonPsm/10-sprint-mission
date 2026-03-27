ec32736701eb35c68c873c524682d8fe159ed5c2
raonPsm <s102075@naver.com>
2026-03-27 16:48:01 +0900

    fix: 유저 정보 일부 수정이 되지 않던 문제 해결
    UserUpdateRequest에 @NotBlank를 설정하여 부분 수정이 되지 않던 문제를 해당 어노테이션을 제거함으로서 해결

--- src/main/java/com/sprint/mission/discodeit/dto/requestRespose/user/UserUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/requestRespose/user/UserUpdateRequest.java
@@ -1,21 +1,18 @@
 package com.sprint.mission.discodeit.dto.requestRespose.user;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 // 정보 수정 시 변경할 정보
+// 부분 수정용 DTO 이므로, @NotBalnk 설정하면 안된다
 public record UserUpdateRequest(
-    @NotBlank(message = "사용자 이름은 필수입니다.")
     @Size(max = 50, message = "사용자 이름은 최대 {max}자까지 입력할 수 있습니다.")
     String newUsername,
 
-    @NotBlank(message = "이메일은 필수입니다.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     @Size(max = 100, message = "이메일 주소는 최대 {mas}자까지 입력할 수 있습니다.")
     String newEmail,
 
-    @NotBlank(message = "비밀번호는 필수입니다.")
     @Size(min = 8, max = 60, message = "비밀번호는 {min}자 이상 {max}자 이하이어야 합니다.")
     String newPassword
     // BinaryContentRequest profileImage
