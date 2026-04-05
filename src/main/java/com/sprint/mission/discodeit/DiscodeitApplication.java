9568004769e53dc9556036edd106615287433577
raonPsm <s102075@naver.com>
2026-04-05 10:35:06 +0900

    feat: UserRepositoryTest 구현
    - UserRepository의 슬라이스 테스트 구현
    - application-test.yml 생성 및 설정

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -2,12 +2,11 @@ package com.sprint.mission.discodeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class DiscodeitApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DiscodeitApplication.class, args);
-    }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiscodeitApplication.class, args);
+  }
 }
