8de88e97b3281b2f5062822dde416952dda79463
raonPsm <s102075@naver.com>
2026-05-14 11:02:52 +0900

    chore: sprint9 베이스 코드 일부 적용 2차

--- src/main/resources/Spring+Mission+FE+2.0.7/0-sprint-mission-front-2.0.7/src/components/Channel/ChannelHeader.tsx
+++
@@ -1,9 +0,0 @@
-import {StyledChannelHeader} from './styles.ts';
-
-export function ChannelHeader(): JSX.Element {
-  return (
-      <StyledChannelHeader>
-        채널 목록
-      </StyledChannelHeader>
-  );
-} 
